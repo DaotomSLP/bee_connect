8ef28e321e752757cd40a5a8372823fe11995de8
Daotom.litd <daotom.litd@gmail.com>
2021-12-17 16:02:42 +0700

    update login design page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,38 +3,42 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="col-lg-4 col-md-6 col-sm-8">
+            <div class="card mt-5 p-lg-5 p-sm-2">
+
+                <p class="h2 text-center font-weight-bold">
+                    {{ __('Login') }}
+                </p>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                            <div class="col-md-12 form-group has-feedback">
+                                <input type="email" id="email"
+                                    class="form-control has-feedback-left @error('email') is-invalid @enderror"
+                                    placeholder="First Name" name="email" value="{{ old('email') }}" required
+                                    autocomplete="email" autofocus>
+                                <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                            <div class="col-md-12 form-group has-feedback">
+                                <input type="password"
+                                    class="form-control has-feedback-left @error('password') is-invalid @enderror"
+                                    placeholder="Password" name="password" required autocomplete="current-password">
+                                <span class="fa fa-lock form-control-feedback left" aria-hidden="true"></span>
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -52,8 +56,8 @@
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary w-100">
                                     {{ __('Login') }}
                                 </button>
                             </div>
