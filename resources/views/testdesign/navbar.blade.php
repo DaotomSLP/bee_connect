8e3da4ca2d98ef59cae695b48ae0c97b7cb11942
Daotom.litd <daotom.litd@gmail.com>
2022-12-25 15:39:06 +0700

    navbar design

--- resources/views/testdesign/navbar.blade.php
+++ resources/views/testdesign/navbar.blade.php
@@ -2,13 +2,13 @@
 <section class="ftco-section">
     <div class="container-fluid">
         <div class="row justify-content-between" style="background-color: black">
-            <div class="col-md-8 order-md-last">
+            <div class="col-md-12 order-md-last">
                 <div class="row">
                     <div class="col-md-6 text-center">
                         <a class="navbar-brand" href="index.html"><img src="{{ URL::asset('/img/design/JSK-logo.jpeg') }}"
                                 class="img-fluid" style="max-height: 50px"></a>
                     </div>
-                    <div class="col-md-6 d-md-flex justify-content-end mb-md-0 mb-3">
+                    <div class="col-md-6 d-none d-md-flex justify-content-end mb-md-0 mb-3 align-items-center">
                         <form action="#" class="searchform order-lg-last">
                             <div class="form-group d-flex">
                                 <input type="text" class="form-control pl-3" placeholder="Search">
@@ -19,22 +19,13 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-4 d-flex">
-                <div class="social-media">
-                    <p class="mb-0 d-flex">
-                        <a href="#" class="d-flex align-items-center justify-content-center"><span
-                                class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-                    </p>
-                </div>
-            </div>
         </div>
     </div>
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div class="container-fluid">
-
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
                 aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="fa fa-bars"></span> Menu
+                <span class="fa fa-bars text-white"></span>
             </button>
             <div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav m-auto">
@@ -56,6 +47,20 @@
             </div>
         </div>
     </nav>
+    <div id="social-float-div" class="card card-body p-2 bg-dark rounded-0">
+        <div class="social-media mb-2">
+            <p class="mb-0 d-flex">
+                <a href="https://www.facebook.com/Jskgroup.lao" target="_blank" class="d-flex align-items-center justify-content-center"><span
+                        class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+            </p>
+        </div>
+        <div class="social-media mb-2">
+            <p class="mb-0 d-flex">
+                <a href="#" class="d-flex align-items-center justify-content-center"><span
+                        class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+            </p>
+        </div>
+    </div>
     <!-- END nav -->
 
 </section>
